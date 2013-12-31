1bbafb72f94da0d6c407484517a2136298bc3e5b
Paul Kaiser <paul@pixotech.com>
2013-12-31 09:30:23 -0600

    Trying to fix animation of tasks

--- client/views/tasks/task_item.js
+++ client/views/tasks/task_item.js
@@ -18,6 +18,28 @@ Template.taskItem.helpers({
   }
 });
 
+Template.taskItem.rendered = function(){
+  // animate task from previous position to new position
+  var instance = this;
+  var rank = instance.data._rank;
+  var $this = $(this.firstNode);
+  var taskHeight = 80;
+  var newPosition = rank * taskHeight;
+  // if element has a currentPosition (i.e. it's not the first ever render)
+  if (typeof(instance.currentPosition) !== 'undefined') {
+    var previousPosition = instance.currentPosition;
+    // calculate difference between old position and new position and send element there
+    var delta = previousPosition - newPosition;
+    $this.css("top", delta + "px");
+  }
+  // let it draw in the old position, then..
+  Meteor.defer(function() {
+    instance.currentPosition = newPosition;
+    // bring element back to its new original position
+    $this.css("top",  "0px");
+  });
+};
+
 // events calling server methods for tasks
 Template.taskItem.events({
   'click .upvotable': function(e) {
--- client/views/tasks/tasks_list.js
+++ client/views/tasks/tasks_list.js
@@ -1,6 +1,13 @@
 // TRUE if current limit == total tasks fetchable
 // this.tasks.count() may work soon, but buggy right now so fetch().length instead
 Template.tasksList.helpers({
+  tasksWithRank: function() {
+    this.tasks.rewind();
+    return this.tasks.map(function(task, index, cursor) {
+      task._rank = index;
+      return task;
+    });
+  },
   hasMoreTasks: function(){
     this.tasks.rewind();
     return Router.current().limit() == this.tasks.fetch().length;
